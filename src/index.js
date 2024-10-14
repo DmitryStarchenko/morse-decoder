63e51450d26830c43cdac0118e47c1950ccf13c0
Dmitry <Dololob92@gmail.com>
2024-10-14 16:12:24 +0300

    test complete

--- src/index.js
+++ src/index.js
@@ -38,7 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let i = 0;
+    let h = 0;
+    let k = 0;
+    let start = 0;
+    let finish = 10;
+    let str = '';
+    let res = '';
+    let result = '';
+    while (i < expr.length / 10) {
+      str = expr.slice(start, finish)
+      start += 10;
+      finish += 10;
+      num = Number(str);
+      str = String(num);
+      while (k < (str.length / 2)) {
+        if (str.slice(h, h + 2) === '10') {
+          result = `${result}${'.'}`;
+          k++;
+          h += 2;
+        } else if (str.slice(h, h + 2) === '11') {
+          result = `${result}${'-'}`;
+          k++;
+          h += 2;
+        } else {
+          result = `${result}${' '}`;
+          k = 5;
+        }
+      }
+      res = `${res}${MORSE_TABLE[result]}`
+      i++;
+      k = 0;
+      h = 0;
+      result = '';
+    }
+    res = res.split('undefined').join(' ');
+    return res;
 }
 
 module.exports = {
